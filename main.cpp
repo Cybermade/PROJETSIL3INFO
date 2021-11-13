631de70ba14b021d146b1c54f34a2cc5d3269fde
Bénas Thomas <benas.thomas@gmail.com>
2021-11-13 13:45:42 +0100

    [add] lumières

--- main.cpp
+++ main.cpp
@@ -192,10 +192,32 @@ int main(int argc,char **argv)
     glutCreateWindow("Projet");
 
     /* Initialisation d'OpenGL */
-    glClearColor(0.0,0.0,0.0,0.0);
+    glClearColor(0.0,0.0,0.0,0);
     glColor3f(1.0,1.0,1.0);
     glPointSize(2.0);
+
+//light
+    GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+    GLfloat mat_shininess[] = { 50.0 };
+    GLfloat light_position[] = { 1.0, 1.0, 1.0, 0.0 };
+    glClearColor (0.0, 0.0, 0.0, 1.0);
+    glShadeModel (GL_SMOOTH);
+
+    glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+    glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+    glMaterialf( GL_FRONT_AND_BACK, GL_AMBIENT, 1);
+    glMaterialf( GL_FRONT_AND_BACK, GL_DIFFUSE, 0.8);
+    glMaterialf( GL_FRONT_AND_BACK, GL_EMISSION, 0.0);
+
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+//end light
+
     glEnable(GL_DEPTH_TEST);
+    glEnable(GL_COLOR_MATERIAL);
+    glEnable( GL_NORMALIZE );
 
     /* Mise en place de la projection perspective */
     glMatrixMode(GL_PROJECTION);
@@ -344,11 +366,13 @@ void affichage()
     glShadeModel(GL_SMOOTH);
 
     glLoadIdentity();
+
+
+
     //Initialisation de la camera
     gluLookAt(posCameraCurrent.x,posCameraCurrent.y,posCameraCurrent.z,posCameraLookAt.x,posCameraLookAt.y,posCameraLookAt.z,0,1,0);
     glRotatef(angley,1.0,0.0,0.0);
     glRotatef(anglex,0.0,1.0,0.0);
-
 
 
 
